e4596068630725c96f3adda7730c5117bfdcf35e
LukaCoder <lukasenashvili@gmail.com>
2023-06-17 14:34:55 +0400

    fixed typescript errors

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,15 +1,17 @@
-import client from '@/lib/db';
-import NextAuth from "next-auth"
+import prisma from '@/lib/db';
+import NextAuth, { NextAuthOptions } from "next-auth"
 import {PrismaAdapter} from '@auth/prisma-adapter';
 import DiscordProdiver from "next-auth/providers/discord"
+import { Adapter } from 'next-auth/adapters';
 
-export const authOptions = {
-  adapter: PrismaAdapter(client),
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     DiscordProdiver({
-      clientId: process.env.DISCORD_CLIENT_ID!,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+      clientId: process.env.DISCORD_CLIENT_ID ?? "",
+      clientSecret: process.env.DISCORD_CLIENT_SECRET ?? "",
     }),
   ],
 }
-export default NextAuth(authOptions as any)
+
+export default NextAuth(authOptions)
